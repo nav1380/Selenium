0337b39e68f5a9e0a897d4b55ce461c84d13c0ab
Godwy <godwynnejoshuadimayuga@gmail.com>
2026-05-08 16:21:35 +0800

    initial commit

---
+++ src/main/java/pages/LoginPage.java
@@ -0,0 +1,26 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+    WebDriver driver;
+
+    By username = By.id("user-name");
+    By password = By.id("password");
+    By loginBtn = By.id("login-button");
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void enterUsernameAndPassword(String usernameInput, String passwordInput) {
+        driver.findElement(username).sendKeys(usernameInput);
+        driver.findElement(password).sendKeys(passwordInput);
+    }
+
+    public void clickLogin() {
+        driver.findElement(loginBtn).click();
+    }
+
+}
